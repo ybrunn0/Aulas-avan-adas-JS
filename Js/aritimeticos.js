d60ac3c484ff70caa94ef512f8be6918609d679e
root <root@DESKTOP-MN8MH7F>
2024-10-20 16:30:07 -0300

    Modificando operadores

--- Js/aritimeticos.js
+++ Js/aritimeticos.js
@@ -1,24 +1,21 @@
-//var, let, const
-console.log("var, let, const")
+// 
 
+var numero1 = 5;
+var numeoro2 = 3;
 
-// var -> ES5 - CROSS-BROWSER
-let teste =  "minha string"; // JavaScript é uma linguagem fracamente tipada.
-teste = 30;
-console.log(teste);
-console.log(" JavaScript é uma linguagem fracamente tipada.")
+console.log("Adição de 5 + 3 = " + (numero1 + numeoro2))
 
-let teste1 =  "minha string"; // O Let não permite que a mesma variável seja novamente atribuido.
-//let teste1 = 10; // comentando para não aparecer o erro. 
-console.log(teste1);
-console.log("O Let não permite que a mesma variável seja novamente atribuido.")
+var numero1 = 5;
+var numeoro2 = 3;
 
-var teste2 = "Minha string";
-var teste2 = 40  // O var permite por ser de escopo global e isso é um perigo
-console.log(teste2);
-console.log("O var permite por ser de escopo global e isso é um perigo")
+console.log("Adição de 5 + 3 = " + (numero1 + numeoro2))
 
-// "Let" é mais seguro que "Var",  pois não é possível reatribuir o valor a variável.
-console.log("'Let' é mais seguro que 'Var',  pois não é possível reatribuir o valor a variável.")
-const teste4 = "Eu sou imutável";
-console.log(teste4);
+var numero1 = 5;
+var numeoro2 = 3;
+
+console.log("Adição de 5 + 3 = " + (numero1 + numeoro2))
+
+var numero1 = 5;
+var numeoro2 = 3;
+
+console.log("Adição de 5 + 3 = " + (numero1 + numeoro2))
