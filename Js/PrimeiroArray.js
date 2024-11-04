5251a86943c491543b82b381330b931f2c53f1ed
root <root@DESKTOP-MN8MH7F>
2024-11-04 18:32:49 -0300

    Exercício Array

--- Js/PrimeiroArray.js
+++ Js/PrimeiroArray.js
@@ -1,35 +1,35 @@
-var caixaDoNumero = document.getElementById("numero");
-var botaoAdivinhar = document.getElementById("adivinhar");
-var tela = document.getElementById(" tela ");
-var c = tela.getContext ("2d");
+// var caixaDoNumero = document.getElementById("numero");
+// var botaoAdivinhar = document.getElementById("adivinhar");
+// var tela = document.getElementById(" tela ");
+// var c = tela.getContext ("2d");
 
-var segredos = [ 16 , 34, 37, 42, 50,58 ] ;
+// var segredos = [ 16 , 34, 37, 42, 50,58 ] ;
 
-var botaoClicado = function(){
-for(var i = 0; i < segredos.length; i = i + 1 ){
-       if(segredos[i] === parseInt(caixaDoNumero.value)){
-           alert("Parabéns! Você acertou o número secreto!");
-           return;
-       }
-   }
-   alert("Infelizmente você errou!");
-};
-botaoAdivinhar.onclick = botaoClicado;
+// var botaoClicado = function(){
+// for(var i = 0; i < segredos.length; i = i + 1 ){
+//        if(segredos[i] === parseInt(caixaDoNumero.value)){
+//            alert("Parabéns! Você acertou o número secreto!");
+//            return;
+//        }
+//    }
+//    alert("Infelizmente você errou!");
+// };
+// botaoAdivinhar.onclick = botaoClicado;
 
-c.fillStyle="green";
-c.fillRect(0, 0, 200, 400 );
+// c.fillStyle="green";
+// c.fillRect(0, 0, 200, 400 );
 
-c.fillStyle="red";
-c.fillRect(400, 0, 200, 400 );
+// c.fillStyle="red";
+// c.fillRect(400, 0, 200, 400 );
 
-c.fillStyle="gray";
-c.beginPath ( );
-c.moveTo(300, 200 );
-c.lineTo (200, 400 );
-c.lineTo (400, 400 );
-c.fill();
+// c.fillStyle="gray";
+// c.beginPath ( );
+// c.moveTo(300, 200 );
+// c.lineTo (200, 400 );
+// c.lineTo (400, 400 );
+// c.fill();
 
-c.fillStyle="blue";
-c.beginPath ( );
-c. arc (300 , 200, 50, 0, 2*3.14 );
-c.fill();
+// c.fillStyle="blue";
+// c.beginPath ( );
+// c. arc (300 , 200, 50, 0, 2*3.14 );
+// c.fill();
